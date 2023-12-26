c73a0d494f2a20eef75b8a2ffeeacc9017e2acae
Replit user <>
2023-12-26 13:11:24 +0300

    исправил функции get_duration

--- datacenter/get_duration.py
+++ datacenter/get_duration.py
@@ -1,9 +1,11 @@
 from django.utils.timezone import localtime
 
-def get_duration(visits):
-    for visitors in visits:
-        enter = visitors.entered_at
-        leave = visitors.leaved_at
-        duration = localtime(leave) - localtime(enter)
-
-        return duration
+def get_duration(visit):
+  enter = visit.entered_at
+  leave = visit.leaved_at
+  if leave:
+    duration = localtime(leave) - localtime(enter)
+    return duration
+  else:
+    duration = localtime() - localtime(enter)
+    return duration
