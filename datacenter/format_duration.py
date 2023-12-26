66334a559b575fadde8108b09e5f05992f89d24d
Replit user <>
2023-12-26 13:25:18 +0300

    Update format_duration.py

--- datacenter/format_duration.py
+++ datacenter/format_duration.py
@@ -2,8 +2,8 @@ from django.utils.timezone import localtime
 
 def format_duration(duration):
     SEC_IN_HOUR = 3600
-    MIN_IN_HOUR = 60
+    SEC_IN_MIN = 60
     hours = round(duration.total_seconds() // SEC_IN_HOUR)
-    minutes = round(duration.total_seconds() // MIN_IN_HOUR)
+    minutes = round(duration.total_seconds() // SEC_IN_MIN) % 60
     time = f'{hours}ч {minutes}мин'
     return time
