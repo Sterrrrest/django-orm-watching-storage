bc7ade3b1dc4862db1032e5cc2bf7ef05533980e
Replit user <>
2023-12-29 13:39:23 +0300

    исправил регистр букв

--- datacenter/functions.py
+++ datacenter/functions.py
@@ -1,10 +1,10 @@
 from django.utils.timezone import localtime
 
 def format_duration(duration):
-    SEC_IN_HOUR = 3600
-    SEC_IN_MIN = 60
-    hours = round(duration.total_seconds() // SEC_IN_HOUR)
-    minutes = round(duration.total_seconds() // SEC_IN_MIN) % 60
+    sec_in_hour = 3600
+    sec_in_min = 60
+    hours = round(duration.total_seconds() // sec_in_hour)
+    minutes = round(duration.total_seconds() // sec_in_min) % sec_in_min
     time = f'{hours}ч {minutes}мин'
     return time
 
