ac49dffd65fef3ef50bde059efecea5706cbdbe6
Replit user <>
2024-01-06 12:50:56 +0300

    Удалил неиспользуемый код и поправил множественное число

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -7,14 +7,13 @@ from functions import get_duration
 from functions import format_duration
   
 def storage_information_view(request):
-    visits = Visit.objects.all()
     visits_not_leaved = Visit.objects.filter(leaved_at=None)
     non_closed_visits = []
   
-    for visitors in visits_not_leaved:
-        who_entered = visitors.passcard.owner_name
-        enter = visitors.entered_at
-        duration = format_duration(get_duration(visitors))
+    for visit in visits_not_leaved:
+        who_entered = visit.passcard.owner_name
+        enter = visit.entered_at
+        duration = format_duration(get_duration(visit))
         non_closed_visits = {
                 'who_entered': who_entered,
                 'entered_at': enter,
