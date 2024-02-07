e71fd9e45ef36df1bc757c9c4130813817443019
Replit user <>
2024-02-07 10:55:51 +0700

    Добавил расширение для get object 404

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,7 +3,7 @@ import datetime
 from django.http import Http404
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 from datacenter.functions import get_duration
 from datacenter.functions import format_duration
